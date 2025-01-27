187306a9a61251f661ccac8faf3357cbf714e0c1
necati <necatibektass@icloud.com>
2025-01-27 18:40:09 +0300

    Adding Visual Feedback to the Buttons
    "Added visual feedback to buttons for better user interaction."
    Improving the Buttons
    
    "Improved button design and responsiveness for a smoother experience."
    Coloring the Components & The Overall App
    
    "Added color scheme to components and overall app for a more engaging UI."
    Adding a Linear Gradient
    
    "Implemented linear gradient for enhanced visual appeal."
    Adding a Background Image
    
    "Set background image to improve the app's aesthetic."
    Getting Started with the Game Logic
    
    "Started implementing basic game logic and functionality."
    Handling User Input & Showing an Alert Dialog
    
    "Handled user input and added alert dialog for feedback on actions."

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,12 +1,40 @@
-import {StyleSheet, TextInput, View} from "react-native";
+import {useState} from "react";
+import {StyleSheet, TextInput, View, Alert} from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
+    const [enteredNumber,setEnteredNumber] = useState('');
+
+    function numberInputHandler(enteredText) {
+        setEnteredNumber(enteredText);
+    }
+
+    function resetInputHandler() {
+        setEnteredNumber('');
+    }
+
+    function confirmInputHandler() {
+        const chosenNumber = parseInt(enteredNumber);
+        if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+            Alert.alert('Invalid number!', 'Number has to be a number between 1 and 99.', [{text: 'Okay', style: 'destructive', onPress: () => resetInputHandler}]);
+            return;
+        }
+
+        console.log('Valid number: ' + chosenNumber);
+    }
+
     return (
         <View style={styles.inputContainer}>
-            <TextInput style={styles.numberInput} maxLength={2} keyboardType="number-pad" autoCapitalize="none" autoCorrect={false} />
-            <PrimaryButton>Reset</PrimaryButton>
-            <PrimaryButton>Confirm</PrimaryButton>
+            <TextInput style={styles.numberInput} maxLength={2} keyboardType="number-pad" autoCapitalize="none"
+                       autoCorrect={false} onChangeText={numberInputHandler} value={enteredNumber} />
+            <View style={styles.buttonsContainer}>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
+                </View>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+                </View>
+            </View>
         </View>
     );
 }
@@ -15,10 +43,12 @@ export default StartGameScreen;
 
 const styles = StyleSheet.create({
     inputContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
         marginTop: 100,
         marginHorizontal: 24,
         padding: 16,
-        backgroundColor: "#72063c",
+        backgroundColor: "#4e0329",
         borderRadius: 8,
         elevation: 4,
         shadowColor: 'black',
@@ -28,7 +58,7 @@ const styles = StyleSheet.create({
     },
     numberInput: {
         height: 50,
-        width:50,
+        width: 50,
         fontSize: 32,
         borderBottomColor: "#ddb52f",
         borderBottomWidth: 2,
@@ -36,5 +66,11 @@ const styles = StyleSheet.create({
         marginVertical: 8,
         fontWeight: 'bold',
         textAlign: 'center'
+    },
+    buttonsContainer: {
+        flexDirection: 'row',
+    },
+    buttonContainer: {
+        flex: 1
     }
 });
